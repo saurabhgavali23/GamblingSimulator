2cc963e6cfb76910d1f830292ca8a6898f0796db
saurabhgavali23 <gavalisaurabh02@gmail.com>
2019-12-11 12:41:12 +0530

    ADDED: Calc Daily Win or Loss per

--- GamblerSimulator.sh
+++ GamblerSimulator.sh
@@ -4,12 +4,29 @@ echo "Welcome Gambler Game"
 BET=1
 STAKE=100
 
-winloss=$((RANDOM%2))
+cash=0
 
-case $winloss in
+lowLimit=$(($STAKE/2))
+highLimit=$(($STAKE + $lowLimit))
 
-	1)
-		echo "win";;
-	0)
-		echo "loss";;
-esac
+function gamblingForDay()
+{
+	cash=$STAKE
+while (( $cash > $lowLimit && $cash < $highLimit ))
+do
+
+	winloss=$((RANDOM%2))
+
+		case $winloss in
+
+		1)
+			cash=$(($cash+1));;
+		0)
+			cash=$(($cash-1));;
+		esac
+done
+
+echo $cash
+}
+
+gamblingForDay

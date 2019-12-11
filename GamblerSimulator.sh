ce1cf039e27d8ba2ea76a24e6a1a01a7f8a20870
saurabhgavali23 <gavalisaurabh02@gmail.com>
2019-12-11 15:23:50 +0530

    ADDED: Fun to Calc 20 Days total Amt

--- GamblerSimulator.sh
+++ GamblerSimulator.sh
@@ -5,28 +5,44 @@ BET=1
 STAKE=100
 
 cash=0
+totalDay=20
+day=0
+win=0
+loss=0
 
 lowLimit=$(($STAKE/2))
 highLimit=$(($STAKE + $lowLimit))
 
-function gamblingForDay()
-{
-	cash=$STAKE
-while (( $cash > $lowLimit && $cash < $highLimit ))
+declare -A totalAmt
+
+function gamblingForDay(){
+
+for (( day=1;day<=20;day++ ))
 do
 
-	winloss=$((RANDOM%2))
+		cash=$STAKE
+	while (( $cash > $lowLimit && $cash < $highLimit ))
+	do
 
+		winloss=$((RANDOM%2))
 		case $winloss in
 
 		1)
-			cash=$(($cash+1));;
+			cash=$(($cash+1))
+			win=$(($win+1));;
 		0)
-			cash=$(($cash-1));;
+			cash=$(($cash-1))
+			loss=$(($loss+1));;
 		esac
-done
+	done
 
-echo $cash
+	totalAmt["Day_$day"]=$win" "$loss
+done
 }
 
 gamblingForDay
+
+for result in ${!totalAmt[@]}
+do
+	echo $result "${totalAmt[$result]}"
+done | sort -k2 -n

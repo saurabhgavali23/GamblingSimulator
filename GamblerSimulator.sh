64e8a455e6c19139176aabb0b36063c198235875
saurabhgavali23 <gavalisaurabh02@gmail.com>
2019-12-13 14:49:32 +0530

    MODIFIED: getLuckyUnLuckyDay function and Variables Names

--- GamblerSimulator.sh
+++ GamblerSimulator.sh
@@ -5,38 +5,40 @@ BET=1
 STAKE=100
 WIN=1
 LOSS=0
+TOTAL_DAY=20
 
 cash=0
-totalDay=20
 day=0
 
-lowLimit=$(($STAKE/2))
-highLimit=$(($STAKE + $lowLimit))
+lowLimit=$(($STAKE - (50*$STAKE) / 100 ))
+highLimit=$(($STAKE + (50*$STAKE) / 100 ))
 
 declare -A totalAmt
 
 function gamblingForDay(){
 
-for (( day=1;day<=20;day++ ))
-do
-
-	cash=$STAKE
-	while (( $cash > $lowLimit && $cash < $highLimit ))
+	for (( day=1;day<=$TOTAL_DAY;day++ ))
 	do
 
-		winloss=$((RANDOM%2))
-		case $winloss in
+		cash=$STAKE
+		while (( $cash > $lowLimit && $cash < $highLimit ))
+		do
 
-		$WIN)
-			cash=$(($cash+1));;
-		$LOSS)
-			cash=$(($cash-1));;
-		esac
+			winLoss=$((RANDOM%2))
+			case $winLoss in
+
+			$WIN)
+				cash=$(($cash+$BET));;
+
+			$LOSS)
+				cash=$(($cash-$BET));;
+
+			esac
+		done
+
+		winLossDiff=$(($cash-$STAKE))
+		totalAmt["Day_$day"]=$winLossDiff
 	done
-
-	winLossDiff=$(($cash-$STAKE))
-	totalAmt["Day_$day"]=$winLossDiff
-done
 }
 
 function getDailyAmt(){
@@ -49,31 +51,34 @@ function getDailyAmt(){
 
 function getLuckyUnLuckyDay(){
 
-	totalAmt[Day_0]=0
 	for ((i=1;i<=20;i++))
 	do
 
-		k=$(( $i - 1 ))
-		totalAmt[Day_$i]=$(( ${totalAmt[Day_$i]} + ${totalAmt[Day_$k]} ))
-		echo "Day$i ${totalAmt[Day_$i]}"
+			val=$(( $i + 1 ))
+			if (( val <= 20 ))
+			then
+				totalAmt[Day_$i]=$(( ${totalAmt[Day_$i]} + ${totalAmt[Day_$val]} ))
+			fi
+			echo "Day$i ${totalAmt[Day_$i]}"
+
 	done | sort -k2 -nr | awk 'NR==20{print "UnLucky " $0}AND NR==1{print "Lucky " $0}'
 }
 
-function playNxtMnthOrNot()
-{
+function playNxtMnthOrNot(){
+
 	if (( $1 <= 0 ))
 	then
 		main
 	fi
 }
 
-function main()
-{
-gamblingForDay
-getDailyAmt
-totalAmt=$( printf "%d\n" ${totalAmt[@]} | awk '{sum+=$0}END{print sum}' )
-playNxtMnthOrNot $totalAmt
-getLuckyUnLuckyDay
+function main(){
+
+	gamblingForDay
+	getDailyAmt
+	totalAmt=$( printf "%d\n" ${totalAmt[@]} | awk '{sum+=$0}END{print sum}' )
+	playNxtMnthOrNot $totalAmt
+	getLuckyUnLuckyDay
 }
 
 main
